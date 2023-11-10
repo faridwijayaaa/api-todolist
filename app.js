ab5f32b760970ec11986c05a3155a43ae4367ef1
faridwijayaaa <faridwijayaaa@gmail.com>
2023-11-10 20:34:20 +0700

    migrate user

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
+const routeAll = require("./routes");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/", (req, res) => {
-  res.json("Sucessfully server running");
-});
+app.use(routeAll);
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
