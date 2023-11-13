bbbd7a55d453c23f2ca138f1d21dd24580bb6158
faridwijayaaa <faridwijayaaa@gmail.com>
2023-11-13 14:05:29 +0700

    first commit

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    username: "root",
+    password: null,
+    database: "test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
@@ -15,10 +15,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB,
   },
 };
